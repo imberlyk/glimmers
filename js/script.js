38757b2b349d381972b25229885d4502e538c617
Kim <77022750+imberlyk@users.noreply.github.com>
2024-08-31 12:55:11 -0400

    djf

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // Function for accordion
-  $('.accordion-title').click(function(){
+  $('.accordion-title').click(function() {
+    var $accordionContent = $(this).next('.accordion-content');
+    $accordionContent.slideToggle().toggleClass('show');
     $(this).parent().toggleClass('active');
-    $(this).next('.accordion-content').slideToggle();
     $(this).find('.accordion-sign').text($(this).parent().hasClass('active') ? '–' : '+');
   });
-  
+
+  // Functionality for "Zine 1" accordion
+  $('.accordion-btn').click(function() {
+    $(this).toggleClass('active');
+    var panel = $(this).next('.accordion-panel');
+    panel.toggleClass('show');
+  });
+
   // Function for school of glimmers
   $("#school-of-glimmers").addClass("grow");
+
+  // Function to scroll to the top
+  $('.scroll-to-top').click(function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
 });
