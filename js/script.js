f8a3af1662e4974297a35f9291a0712bd6442f3f
Kim <77022750+imberlyk@users.noreply.github.com>
2024-09-02 11:17:50 -0400

    bubu

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 
 (function() {
   const interval = 1000;
-  const timeout = 90;
+  const timeout = 60;
   let idleCounter = 0;
   window.onload = document.onmousemove = document.onkeypress = function() {
     idleCounter = 0;
@@ -42,41 +42,138 @@ $(document).ready(function() {
   }, interval);
 })();
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('footer').outerHeight();
+// Sparkles 
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
+(function($, window, document) {
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+  const defaults = {
+    fill: "#fff",
+    stroke: "#fff",
+    size: 20,
+    delay: 0,
+    duration: 1500,
+    pause: 1000
+  };
+  const optionsKeys = ["fill", "stroke", "size", "delay", "duration", "pause"];
+  
+  const optionsStrToObj = function(optionsStr) {
+    const optionsArr = !!optionsStr ? optionsStr.split(" ") : [];
+    var optionsObj = {};
+    
+    for (var i=0; i<optionsArr.length; ++i) {
+      optionsObj[optionsKeys[i]] = optionsArr[i];
     }
-}, 250);
+    
+    return optionsObj;
+  };
+  
+  $.fn.sparkle = function(options) {
+    $.destroySparkle = $.destroySparkle || {};
+    
+    const $this = this;
+    const id = this.data("sparkle-id") || (new Date()).getTime() + Math.random();
+    
+    if (options === "destroy" && this.find("svg").length > 0) {
+      $.destroySparkle[id] = true;
+      this.data("sparkle-id", null);
+    }
+    
+    var settings;
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('footer').removeClass('nav-down').addClass('nav-up');
+    if (options instanceof Array) {
+      for (var i=0; i<options.length; ++i) {
+        $this.sparkle(optionsStrToObj(options[i]));
+      }
+      
+      return;
+    } else if (options instanceof Object) {
+      settings = $.extend({}, defaults, options);
     } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('footer').removeClass('nav-up').addClass('nav-down');
-        }
+      settings = defaults;
     }
     
-    lastScrollTop = st;
-}
+    const cssAnimationAttr = "my-sparkle " + settings.duration + "ms infinite linear";
+
+    const $star = $('<svg class="my-sparkle" version="1.1" viewBox="0.0 0.0 50.0 50.0" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><clipPath id="p.0"><path d="m0 0l50.0 0l0 50.0l-50.0 0l0 -50.0z" clip-rule="nonzero"></path></clipPath><g clip-path="url(#p.0)"><path fill="' + settings.stroke + '" fill-opacity="0.0" d="m0 0l50.0 0l0 50.0l-50.0 0z" fill-rule="nonzero"></path><path fill="' + settings.fill + '" d="m0.62204725 25.0l20.068499 -4.323374l4.309454 -20.13332l4.309454 20.13332l20.068499 4.323374l-20.068499 4.323374l-4.309454 20.133318l-4.309454 -20.133318z" fill-rule="nonzero"></path><path stroke="' + settings.stroke + '" stroke-width="1.0" stroke-linejoin="round" stroke-linecap="butt" d="m0.62204725 25.0l20.068499 -4.323374l4.309454 -20.13332l4.309454 20.13332l20.068499 4.323374l-20.068499 4.323374l-4.309454 20.133318l-4.309454 -20.133318z" fill-rule="nonzero"></path></g></svg>').css({
+        position: "absolute",
+        width: settings.size,
+        height: settings.size,
+        zIndex: 9999
+    });
+
+    const w = this.width();
+    const h = this.height();
+
+    const getCoordinates = function() {
+      return {
+        left: Math.random() * w,
+        top: Math.random() * h
+      };
+    };
+
+    const placeStar = function(init) {
+      const coords = getCoordinates();
+
+      if (init) {
+        $this.append($star);
+      }
+
+      $star.css({
+        "-moz-animation": cssAnimationAttr,
+        "-webkit-animation": cssAnimationAttr,
+        animation: cssAnimationAttr,
+        display: "block",
+        left: coords.left,
+        top: coords.top
+      });
+
+      window.setTimeout(function() {
+        $star.css({
+          "-moz-animation": null,
+          "-webkit-animation": null,
+          animation: null,
+          display: "none"
+        });
+        
+        if (!$.destroySparkle[id]) {
+          window.setTimeout(function() {
+            placeStar(false);
+          }, settings.pause);
+        } else {
+          $star.remove();
+        }
+      }, settings.duration);
+    };
+
+    if (this.css("position") === "static") {
+      this.css("position", "relative");
+    }
+
+    if (!$.destroySparkle[id] && options !== "destroy") {
+      window.setTimeout(function() {
+        placeStar(true);
+      }, settings.delay);
+
+      this.data("sparkle-id", id);
+    }
+
+    return this;
+  };
+  
+  $("#overlay").sparkle({
+    size: 25,
+  }).sparkle({
+    delay: 1000,
+    pause: 750,
+    size: 10
+  });
+
+  /*window.setTimeout(function() {
+    $("#main").sparkle("destroy");
+  }, 21000);*/
+
+})(jQuery, window, document);
+
+
+
+
